745da441b7813f7cae53ca0b93c775d686baff3c
Alin Spinu <spinualin@gmail.com>
2013-05-15 18:28:58 +0300

    Extending Backbone.js with a custom Widget Class
    Widget features:
    - subview management,
    - templating engines
    - model update bind

---
+++ js/modules/controllers/MyController.js
@@ -0,0 +1,31 @@
+define([
+ 'underscore', 
+ 'backbone',
+ 'views/MyView',
+ 'widget'
+
+], function(_, Backbone, MyView, Widget){
+	/**
+	 * Controller Object responsible for View construction and application event flow
+	 * @type {[Object]}
+	 */
+	var MyController = _.extend( {}, Backbone.Events );
+	/**
+	 * Define application logic here, by extending the Controller
+	 */
+	_.extend( MyController, {
+		/**
+		 * Function called immediately after App Initialize 
+		 */
+		start: function(){
+
+			console.log('Controller::Start  --> define logic');		
+			console.log(new Widget())
+
+		} // end start
+
+	});
+
+	return MyController;
+
+});
---
+++ main.js
@@ -0,0 +1,36 @@
+/**
+ * Configure application modules paths
+ */
+require.config({
+	baseUrl: 'js',
+	paths: {
+		jquery: 'library/min/jquery',
+	    underscore: 'library/min/underscore',
+	    backbone: 'library/min/backbone',
+	    templates: 'templates',
+	    controllers: 'modules/controllers',
+	    models: 'modules/models',
+	    views: 'modules/views',
+	    modules: 'modules',
+	    widget: 'modules/views/widget-0.7',
+	    transit: 'library/min/jquery.transit.min'
+	}
+});
+/**
+ * Load main App module
+ */
+require([
+	'app'
+	
+
+], function(App, tansit){ 
+	/**
+	 *  Initialize application when DOM finishes loading
+	 */
+	$(function(){
+
+		window.App = App.initialize();
+
+	});
+
+});
